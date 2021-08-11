754d88688c938bff99063a28b368befb910ea527
Annapurna Nyshadham <annapurna.nyshadham@gmail.com>
2021-08-11 20:49:44 +0530

    Compute word count per text-area and add them all

--- main.js
+++ main.js
@@ -1,61 +1,64 @@
-
-// returns a list of strings from strings in all text boxes
-function getTextBoxesString() {
+// splits string from text box, stores length of resultant list in 'listOfCounts' list
+// input --> string from each textarea
+// returns list of word counts
+function eachWordcount() {
     let collectiveTextboxes = document.getElementsByClassName("input-box");
-    let finalString = "";
+    let listOfCounts = [];
     for(let i = 0; i < collectiveTextboxes.length; i++) {
-        finalString = finalString + " " + collectiveTextboxes[i].value.trim();
-    }
-    let splitString = finalString.split(/\s+/);
-    return splitString;
-}
-
-// returns the number of strings in the list of strings
-function countWords(inputList) {
-    let wordCount = 0;
-    for (let i = 0; i < inputList.length; i++) {
-        if (inputList[i] !== "") {
-            wordCount++;
+        if (collectiveTextboxes[i].value !== "") {
+            listOfCounts.push(collectiveTextboxes[i].value.trim().split(/\s+/).length);
         }
     }
-    return wordCount;
+    return listOfCounts;
 }
 
-// updates the value of 'count' dom object
-function updateCount(wordCount) {
+
+// adds all elements of a given list
+// input --> list from eachWordCount()
+// returns sum of all elements
+function totalWordCount(listOfWordCounts) {
+    let sum = 0;
+    for (i = 0; i < listOfWordCounts.length; i++) {
+        sum = sum + listOfWordCounts[i];
+    }
+    return sum;
+}
+
+
+// updates 'count' dom element
+// input --> number; sum of no. of words from all textareas
+function updateCount(sum) {
     let count = document.getElementById("count");
-    count.innerText = wordCount;
+    count.innerText = sum;
 }
 
-// event listener
+// calls all functions
 function finalCall() {
-    let step1 = getTextBoxesString();
-    let step2 = countWords(step1);
+    let step1 = eachWordcount();
+    console.log("step1", step1);
+    let step2 = totalWordCount(step1);
+    console.log("step2", step2);
     updateCount(step2);
-    console.log("run in final call");
 }
 
+// 'Generate' button event handler
 let submitButton = document.getElementById("submit-button");
 submitButton.addEventListener("click", () => {
     let a = document.getElementById("textbox-number");
     let b = Number(a.value);
-    var c = '';
+    let c = '';
     for (j = 0; j < b; j++) {
-        var textbox = '<textarea class="input-box"></textarea>';
+        let textbox = '<textarea class="input-box"></textarea>';
         c = c + textbox;
     }
 
     let d = document.getElementById("text-areas");
     d.innerHTML = c;
-    //d.addEventListener("keyup", finalCall);
+
     let collectiveTextboxes = document.getElementsByClassName("input-box");
     for(let i = 0; i < collectiveTextboxes.length; i++) {
         collectiveTextboxes[i].addEventListener("keyup", finalCall);
     }
-    //console.log(b);
 });
 
-// registering event listener
-
-//console.log("last line");
 document.addEventListener("DOMContentLoaded", finalCall);
